7f2333eeddd28f6d9b20502881463fd2760dc88a
anton <kamaeff2@gmail.com>
2024-07-19 23:41:41 +0300

    anim

--- src/components/Home/header/HeaderTemplate.vue
+++ src/components/Home/header/HeaderTemplate.vue
@@ -65,121 +65,122 @@ onUnmounted(() => {
     </div>
   </header>
 
-  <div v-if="isModalOpen"
-       class="modal"
-       @click.stop>
+  <Transition name="bounce">
+    <div v-if="isModalOpen"
+         class="modal"
+         @click.stop>
 
-    <button class="close"
-            @click="closeModal"
-            title="Close">
-      <X />
-    </button>
+      <button class="close"
+              @click="closeModal"
+              title="Close">
+        <X />
+      </button>
+
+      <div class="content">
+        <h2>
+          <CircleUserRound :size="26"
+                           :strokeWidth="1" />
+          {{ username !== '' ? username : 'User' }}
+        </h2>
+
+        <form class="form"
+              @submit.prevent="saveUser">
+
+          <input type="text"
+                 v-model="username"
+                 :disabled="!isEditUser"
+                 :placeholder="username !== '' ? name : 'Paste nickname'"
+                 :class="{ 'scale': isEditUser }" />
+
+          <button type="button"
+                  v-show="!isEditUser"
+                  @click="isEditUser = true"
+                  title="Edit username">
+
+            <FilePenLine />
+          </button>
+
+          <button type="submit"
+                  v-show="isEditUser"
+                  title="Submit">
+
+            <ChevronsRight />
+          </button>
+
+        </form>
+
+        <form class="form"
+              @submit.prevent="saveName">
+
+          <input type="text"
+                 v-model="name"
+                 :disabled="!isEditName"
+                 :placeholder="name !== '' ? name : 'Paste name'"
+                 :class="{ 'scale': isEditName }" />
+
+          <button type="button"
+                  v-show="!isEditName"
+                  @click="isEditName = true"
+                  title="Edit Name">
+
+            <FilePenLine />
+          </button>
+
+          <button type="submit"
+                  v-show="isEditName"
+                  title="Submit">
+
+            <ChevronsRight />
+          </button>
+
+        </form>
 
 
+      </div>
 
-    <div class="content">
-      <h2>
-        <CircleUserRound :size="26"
-                         :strokeWidth="1" />
-        {{ username !== '' ? username : 'User' }}
-      </h2>
+      <div class="stats">
+        <h2>Stats</h2>
+        <p class="name">{{ name !== '' ? name : 'My name' }}</p>
 
-      <form class="form"
-            @submit.prevent="saveUser">
+        <div class="item">
+          <p>
+            <Tally5 :size="16"
+                    color="gray" />
+            Total
+          </p>
+          <span>123</span>
+        </div>
 
-        <input type="text"
-               v-model="username"
-               :disabled="!isEditUser"
-               :placeholder="username !== '' ? name : 'Paste nickname'"
-               :class="{ 'scale': isEditUser }" />
+        <div class="item">
+          <p>
+            <SquareCheckBig :size="16"
+                            color="green" />
+            Done
+          </p>
+          <span>50</span>
+        </div>
 
-        <button type="button"
-                v-show="!isEditUser"
-                @click="isEditUser = true"
-                title="Edit username">
+        <div class="item">
+          <p>
+            <TriangleAlert :size="16"
+                           color="yellow" />
+            InProgress
+          </p>
+          <span>2</span>
+        </div>
 
-          <FilePenLine />
-        </button>
-
-        <button type="submit"
-                v-show="isEditUser"
-                title="Submit">
-
-          <ChevronsRight />
-        </button>
-
-      </form>
-
-      <form class="form"
-            @submit.prevent="saveName">
-
-        <input type="text"
-               v-model="name"
-               :disabled="!isEditName"
-               :placeholder="name !== '' ? name : 'Paste name'"
-               :class="{ 'scale': isEditName }" />
-
-        <button type="button"
-                v-show="!isEditName"
-                @click="isEditName = true"
-                title="Edit Name">
-
-          <FilePenLine />
-        </button>
-
-        <button type="submit"
-                v-show="isEditName"
-                title="Submit">
-
-          <ChevronsRight />
-        </button>
-
-      </form>
+        <div class="item">
+          <p>
+            <X :size="16"
+               color="red" />
+            New
+          </p>
+          <span>56</span>
+        </div>
+      </div>
 
 
+      <p class="warning">Careful! All data will be stored in your browser ;)</p>
     </div>
-
-    <div class="stats">
-      <h2>Stats</h2>
-      <p class="name">{{ name !== '' ? name : 'My name' }}</p>
-
-      <div class="item">
-        <p>
-          <Tally5 :size="16"
-                  color="gray" />
-          Total
-        </p>
-        <span>123</span>
-      </div>
-
-      <div class="item">
-        <p>
-          <SquareCheckBig :size="16"
-                          color="green" />
-          Done
-        </p>
-        <span>50</span>
-      </div>
-
-      <div class="item">
-        <p>
-          <TriangleAlert :size="16"
-                         color="yellow" />
-          InProgress
-        </p>
-        <span>2</span>
-      </div>
-
-      <div class="item">
-        <p>
-          <X :size="16"
-             color="red" />
-          New
-        </p>
-        <span>56</span>
-      </div>
-    </div>
-
-    <p class="warning">Careful! All data will be stored in your browser ;)</p>
-  </div>
+  </Transition>
 </template>
--- src/routes/HomeRoute.vue
+++ src/routes/HomeRoute.vue
@@ -11,7 +11,7 @@ onMounted(() => {
     loader.value = false
 
     console.log('Component mounted');
-  }, 1000)
+  }, 1500)
 
   return () => clearTimeout(timeout)
 })
@@ -27,15 +27,38 @@ onMounted(() => {
 
       <div class="text">
         <p>#TaskList</p>
+
         <p class="sub">Simple and Fast</p>
       </div>
 
     </div>
   </div>
 
-  <div v-show="!loader">
-    <MainTemplate />
-  </div>
-
+  <Transition name="bounce">
+    <div v-show="!loader">
+      <MainTemplate />
+    </div>
+  </Transition>
 
 </template>
+
+
+<style>
+.bounce-enter-active {
+  animation: bounce-in 0.5s;
+}
+
+.bounce-leave-active {
+  animation: bounce-in 0.5s reverse;
+}
+
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
