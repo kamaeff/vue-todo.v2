a85e8d76cc4654213d006fb3dd5ab1ec1a94ed29
Anton Kamaev <99755906+kamaeff@users.noreply.github.com>
2024-07-20 00:56:05 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/vue-todo.v2/sw.js', { scope: '/vue-todo.v2/' })})}
